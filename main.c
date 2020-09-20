c8cdc36a2bb2bd94a60f22a5efc2d71292976a58
Sergio Ruiz <sergio.rruvalcaba@alumnos.udg.mx>
2020-09-20 18:33:31 -0500

    Se captura edad

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main() {
+   int nombre;
    char[100] nombre;
    printf("Hello, World!");
    printf("Nombre:")
    fgets(nombre, sizeof(nombre), stdin);
-   printf("Hola %s", nombre);
+   printf("Edad:")
+   scanf("d", edad);
+   printf("%s tiene %d ", nombre, edad);
    return 0;
 }
